a2fc98cedb733f17c91601a3b061c76670f6188e
Tim Taylor <ttaylor@clarion.edu>
2017-03-01 20:16:13 -0500

    working with April to catch up in testing the board positions

--- board.rb
+++ board.rb
@@ -18,6 +18,14 @@ class Board
 		end
 	end
 
+	def valid_input?(input)
+		if input.match(/[XxOo]/)
+			true
+		else
+			false
+		end
+	end
+
 	def full_board?
 		if ttt_board.include?('') #if there is an empty string/position false, if not true
 			false
@@ -25,5 +33,22 @@ class Board
 			true
 		end
 	end
+
+	def winner?(marker)
+	win_array = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3,6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+	results = false
+		win_array.each do |win|
+			row = 0
+			win.each do |pos|
+				if ttt_board[pos] == marker
+					row += 1
+					if row == 3
+						results = true
+					end
+				end
+			end
+		end
+		results
+	end					
 
 end
--- test_board.rb
+++ test_board.rb
@@ -48,6 +48,36 @@ class TestTicTacToe < Minitest::Test
 		board.ttt_board = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
 		assert_equal(true, board.full_board?)
 	end
+
+	def test_valid_input
+		board = Board.new
+		assert_equal(false, board.valid_input?('m'))
+		assert_equal(true, board.valid_input?('x'))
+		assert_equal(true, board.valid_input?('O'))
+		assert_equal(false, board.valid_input?(''))
+		assert_equal(false, board.valid_input?('@'))
+		assert_equal(true, board.valid_input?('X'))
+	end
+		def test_winner_at_pos_012_with_X
+		board = Board.new
+		marker = 'X'
+		board.ttt_board = ['X', 'X', 'X', 'O', 'X', 'O', 'O', 'X', 'X']
+		assert_equal(true, board.winner?(marker))
+	end
+
+	def test_winner_at_pos_048_with_x
+		board = Board.new
+		marker = 'x'
+		board.ttt_board = ['x', 'x', 'o', 'o', 'x', 'x', 'o', 'o', 'x']
+		assert_equal(true, board.winner?(marker))
+	end
+
+	def test_no_winner
+		board = Board.new
+		marker = 'o'
+		board.ttt_board = ['x', 'o', 'x', 'o', 'o', 'x', 'o', 'x', 'o']
+		assert_equal(false, board.winner?(marker))
+	end
 	
 
 end
