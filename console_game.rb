903056d4b79de46a21ef7985a2e0d9430ddd2f67
Tim Taylor <ttaylor@clarion.edu>
2017-03-02 11:31:14 -0500

    displaying the board

--- console_game.rb
+++ console_game.rb
@@ -15,5 +15,14 @@ class Console_Game
 	def intro_ttt
 		puts 'Welcome to Tic Tac Toe'
 	end
-	
+
+	def display_board
+		puts " #{board.ttt_board[0]} | #{board.ttt_board[1]} | #{board.ttt_board[2]} "
+	 	puts "--------"
+	 	puts " #{board.ttt_board[3]} | #{board.ttt_board[4]} | #{board.ttt_board[5]} "
+	 	puts "--------"
+	 	puts " #{board.ttt_board[6]} | #{board.ttt_board[7]} | #{board.ttt_board[8]} "
+	end
+
+
 end
--- play_console_game.rb
+++ play_console_game.rb
@@ -1,4 +1,7 @@
 require_relative 'console_game.rb'
 
 game = Console_Game.new(Random_AI.new('X'), Random_AI.new('O'))
+
 game.intro_ttt
+
+game.display_board
