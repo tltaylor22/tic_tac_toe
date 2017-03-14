560eda78a64b2c9f466ac966d4539090d98c5be1
Tim Taylor <ttaylor@clarion.edu>
2017-03-14 16:59:49 -0400

    added intro, name and pickgame page with some css style formatting

--- app.rb
+++ app.rb
@@ -6,4 +6,27 @@ class TicTacToeApp < Sinatra::Base
 		erb :intro
 	end
 
+	post '/intro' do
+		yes = params[:yes_select]
+		redirect '/name?'
+	end
+
+	get '/name' do
+		intro = params[:intro]
+		name = params[:name_input]
+		erb :name, locals: {intro: intro}
+	end
+
+	post '/name' do
+		intro = params[:intro]
+		name = params[:name_input]
+		redirect '/pickgame?name=' + name 
+	end
+
+	get '/pickgame' do
+		intro = params[:intro]
+		name = params[:name]
+		game = params[:game_input]
+		erb :pickgame, locals: {intro: intro, name: name}
+	end
 end
