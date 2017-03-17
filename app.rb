5fe35069e36c080a711392506186614e5b53be4d
Tim Taylor <ttaylor@clarion.edu>
2017-03-17 15:43:11 -0400

    trying to pull my player choice for an opponent

--- app.rb
+++ app.rb
@@ -1,4 +1,10 @@
 require 'sinatra'
+require 'rubygems'
+require_relative 'board.rb'
+require_relative 'random_ai.rb'
+require_relative 'sequential_ai.rb'
+require_relative 'human.rb'
+require_relative 'unbeatable_ai.rb'
 
 class TicTacToeApp < Sinatra::Base
 
@@ -14,29 +20,37 @@ class TicTacToeApp < Sinatra::Base
 	get '/name' do
 		intro = params[:intro]
 		name = params[:name_input]
-		erb :name, locals: {intro: intro}
+		erb :name, locals: {intro: intro} # OR is it: locals: => {intro: intro}???
 	end
 
 	post '/name' do
 		intro = params[:intro]
 		name = params[:name_input]
-		redirect '/pickgame?name=' + name 
+		redirect '/pickopp?name=' + name 
 	end
 
-	get '/pickgame' do
+	get '/pickopp' do
 		intro = params[:intro]
 		name = params[:name]
-		game = params[:game_select]
-		erb :pickgame, locals: {intro: intro, name: name}
+		opp = params[:opp_select]
+		erb :pickopp, locals: {intro: intro, name: name}
 	end
 
-	post '/pickgame' do
+	post '/pickopp' do
 		intro = params[:intro]
 		name = params[:name_input]
-		game = params[:game_select]
+		easy = params[:random_ai]
+		medium = params[:sequential_ai]
+		unbeatable = params[:unbeatable_ai]
+		human = params[:human]
 		redirect '/game1?name=' + name
 	end
 
 	get '/game1' do
+		intro = params[:intro]
+		name = params[:name]
+		game = params[:game_select]
+		erb :pickopp, locals: {intro: intro, name: name, game_select: game}
 	end
+
 end
